e588d83e76590613f560a23ef0f01d75e03e1848
hellogaojing <2987966314@qq.com>
2018-05-31 09:09:25 +0800

    gjTest

--- amazingReader/amazingReader/src/com/amazingReader/mapper/BookMapper.java
+++ amazingReader/amazingReader/src/com/amazingReader/mapper/BookMapper.java
@@ -20,4 +20,9 @@ public interface BookMapper {
 	public List<Book> getBooksByLabelId(int LabelId);
 	public void addRecommendTicket(int tickets,int bookId );
 	public String selectBookContent(String content);
+	/**
+	 *
+	 *
+	 * dfbhbd
+	 */
 }
